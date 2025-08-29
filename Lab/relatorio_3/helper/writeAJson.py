875f2f65c5f0a7458f223c4645082331c8f66ba4
D1D104 <diegoresende403@gmail.com>
2025-08-29 14:22:03 -0300

    Finaliza, executa e salva o relatório 3

--- Lab/relatorio_3/helper/writeAJson.py
+++ Lab/relatorio_3/helper/writeAJson.py
@@ -1,15 +1,15 @@
 import json
 import os
-from bson import json_util # pip install bson
+from bson import json_util  # pip install bson
+
 
 def writeAJson(data, name: str):
     parsed_json = json.loads(json_util.dumps(data))
 
-    if not os.path.isdir("./relatório_3"):
-        os.makedirs("./relatório_3")
+    base_path = os.path.join("Lab", "relatorio_3", "json")
 
-    with open(f"./relatório_3/{name}.json", 'w', encoding='utf-8') as json_file:
-        json.dump(parsed_json, json_file,
-                  indent=4,
-                  separators=(',', ': '),
-                  ensure_ascii=False)
+    os.makedirs(base_path, exist_ok=True)
+
+    file_path = os.path.join(base_path, f"{name}.json")
+    with open(file_path, 'w', encoding='utf-8') as json_file:
+        json.dump(parsed_json, json_file, indent=4, separators=(',', ': '))
--- Lab/relatorio_3/main.py
+++ Lab/relatorio_3/main.py
@@ -2,4 +2,43 @@ from database import Database
 from helper.writeAJson import writeAJson
 
 db = Database(database="pokedex", collection="pokemons")
-db.resetDatabase()
+#db.resetDatabase()
+class Pokedex: 
+
+    def __init__(self, db: Database):
+        self.collection = db.collection
+
+    def getPokemonsByCandyCount(self, min_candies: int):
+        return list(self.collection.find({"candy_count": {"$gt": min_candies}}, {"name": 1, "candy_count": 1}))
+
+    def getPokemonsByWeaknesses(self, weaknesses: list[str]):
+        return list(self.collection.find({"weaknesses": {"$all": weaknesses}}, {"name": 1, "weaknesses": 1}))
+    
+    def getPokemonsWithMultipleEvolutions(self):
+        return list(self.collection.find({"next_evolution.1": {"$exists": True}}, {"name": 1, "next_evolution": 1}))
+    
+    def getTopSpawnChance(self, limit: int = 5):
+        return list(self.collection.find({}, {"name": 1, "spawn_chance": 1})
+                    .sort("spawn_chance", -1).limit(limit))
+
+    def getStrongEvolutionPokemons(self):
+        return list(self.collection.find({"next_evolution": {"$exists": True},
+                                          "multipliers": {"$elemMatch": {"$gt": 1.5}}},
+                                         {"name": 1, "multipliers": 1, "next_evolution": 1}))
+
+pokedex = Pokedex(db)
+
+candy_count = pokedex.getPokemonsByCandyCount(50)
+writeAJson(candy_count,"50_candy_to_evolve")
+
+weakness_fire_ice = pokedex.getPokemonsByWeaknesses(["Fire","Ice"])
+writeAJson(weakness_fire_ice, "weakness_fire_ice")
+
+multi_evol = pokedex.getPokemonsWithMultipleEvolutions()
+writeAJson(multi_evol, "multi_evolutions")
+
+top_spawn = pokedex.getTopSpawnChance(10)
+writeAJson(top_spawn, "top_spawn_chance")
+
+strong_evolution = pokedex.getStrongEvolutionPokemons()
+writeAJson(strong_evolution, "strong_evolution")
