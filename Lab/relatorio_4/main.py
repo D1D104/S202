6d6c34eb5548a665c848e31b4370a6bb98047ef8
D1D104 <diegoresende403@gmail.com>
2025-09-02 15:38:37 -0300

    Adiciona classe ProductAnalyzer com pipelines de análise de vendas

--- Lab/relatorio_4/main.py
+++ Lab/relatorio_4/main.py
@@ -2,7 +2,81 @@ from database import Database
 from helper.writeAJson import writeAJson
 
 db = Database(database="mercado", collection="compras")
-db.resetDatabase()
+#db.resetDatabase()
+
+class ProductAnalyzer:
+
+    def __init__(self, db: Database):
+        self.collection = db.collection
+
+    def vendasDia(self):
+        result = self.collection.aggregate([
+            {"$unwind": "$produtos"},
+            {"$group": {
+                "_id": "$data_compra",
+                "total_vendas": {
+                    "$sum": {
+                        "$multiply": ["$produtos.quantidade", "$produtos.preco"]
+                    }
+                }
+            }},
+            {"$sort": {"_id": 1}}
+        ])
+        return list(result)    
+
+    def produtoMaisVendido(self):
+        result = self.collection.aggregate([
+            {"$unwind": "$produtos"},
+            {"$group": {
+                "_id": "$produtos.descricao",
+                "quantidade_total": {"$sum": "$produtos.quantidade"}
+            }},
+            {"$sort": {"quantidade_total": -1}},
+            {"$limit": 1}
+        ])
+        return list(result)
+
+    def clienteMaiorGasto(self):
+        result = self.collection.aggregate([
+            {"$unwind": "$produtos"},
+            {"$group": {
+                "_id": "$_id", 
+                "cliente": {"$first": "$cliente_id"},
+                "total": {
+                    "$sum": {"$multiply": ["$produtos.quantidade", "$produtos.preco"]}
+                }
+            }},
+            {"$sort": {"total": -1}},
+            {"$limit": 1}
+        ])
+        return list(result)
+
+    def produtosAcimaUmaVenda(self):
+        result = self.collection.aggregate([
+            {"$unwind": "$produtos"},
+            {"$group": {
+                "_id": "$produtos.descricao",
+                "quantidade_total": {"$sum": "$produtos.quantidade"}
+            }},
+            {"$match": {"quantidade_total": {"$gt": 1}}},
+            {"$sort": {"quantidade_total": -1}}
+        ])
+        return list(result) 
+
+
+produto = ProductAnalyzer(db)
+
+vendasDia = produto.vendasDia()
+writeAJson(vendasDia, "Vendas por dia")
+
+produtoMaisVendido = produto.produtoMaisVendido()
+writeAJson(produtoMaisVendido, "Produto mais vendido")
+
+clienteMaiorGasto = produto.clienteMaiorGasto()
+writeAJson(clienteMaiorGasto, "Cliente que mais gastou")
+
+produtosAcimaUmaVenda = produto.produtosAcimaUmaVenda()
+writeAJson(produtosAcimaUmaVenda, "Produtos acima de uma unidade")
 
 # 1- Média de gasto total:
 # result = db.collection.aggregate([
