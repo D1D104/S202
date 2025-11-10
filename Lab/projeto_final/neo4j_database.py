965635b8ae0603ccadf30bc55d367aeb465db578
D1D104 <diegoresende403@gmail.com>
2025-11-10 18:40:21 -0300

    fix: drop database when starting application

--- Lab/projeto_final/neo4j_database.py
+++ Lab/projeto_final/neo4j_database.py
@@ -2,13 +2,15 @@ from neo4j import GraphDatabase, Driver
 from typing import List, Dict, Any
 
 class Neo4jDatabase:
-    def __init__(self, uri: str = "bolt://localhost:7687", user: str = "neo4j", password: str = "test"):
+    def __init__(self, uri: str = "bolt://localhost:7687", user: str = "neo4j", password: str = "neo4jneo4j"):
         try:
             self.driver: Driver = GraphDatabase.driver(uri, auth=(user, password))
             print(f"[Neo4j] Conectado a {uri} como {user}")
         except Exception as e:
             print("[Neo4j] Erro ao conectar:", e)
             raise
+
+        self.drop_all()
 
     def close(self):
         self.driver.close()
@@ -19,7 +21,6 @@ class Neo4jDatabase:
         with self.driver.session() as session:
             results = session.run(query, parameters)
             for record in results:
-                # convert Record to dict
                 data.append(record.data())
         return data
 
